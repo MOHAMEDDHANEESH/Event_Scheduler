b7b198eafa431f9294fd016282a128da1c4a01ce
Mohamed Dhaneesh M <mohameddhaneesh79@gmail.com>
2026-01-08 10:24:22 +0530

    Delete Airele Project/event_scheduler directory

--- Airele Project/event_scheduler/db.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-def get_db_connection():
-    return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="Dhaneesh@123",
-        database="event_scheduler"
-    )
